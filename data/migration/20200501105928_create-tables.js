72e3ecda776e76ef5a482eac7bb2a05fe5a363dd
Logan Negley <logannegley@gmail.com>
2020-05-01 11:13:34 -0500

    tables created in migrations file

--- data/migration/20200501105928_create-tables.js
+++ data/migration/20200501105928_create-tables.js
@@ -1,6 +1,30 @@
 
 exports.up = function(knex) {
-  
+  return (
+      knex.schema
+        .createTable('recipes', tbl =>{
+            tbl.increments();
+            tbl.string('recipe_name').notNullable().unique();
+        })
+        .createTable('ingredients', tbl =>{
+            tbl.increments();
+            tbl.string('name').notNullable();
+            tbl.string('quantity').notNullable();
+            tbl.integer('recipe_id')
+                .unsigned()
+                .notNullable()
+                .references('id')
+                .inTable('recipes');
+        })
+        .createTable('steps', tble =>{
+            tbl.increments();
+            tbl.string('instructions').notNullable();
+            tbl.integer('recipe_id')
+                .unsigned()
+                .notNullable()
+                .references('recipe.id');
+        })
+  );
 };
 
 exports.down = function(knex) {
