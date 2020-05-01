f60dfc8e8375e36da561529ed12985589156a7fd
Logan Negley <logannegley@gmail.com>
2020-05-01 12:27:49 -0500

    recipe and ingredients seeds populated

--- data/seeds/02-ingredients.js
+++ data/seeds/02-ingredients.js
@@ -1,13 +1,11 @@
 
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+exports.seed = function(knex, Promise) {
+  return knex('table_name').insert([
+    {id: 1, name: 'cereal', quantity: '1 cup', recipe_id: 1},
+    {id: 2, name: 'milk', quantity: '1 cup', recipe_id: 1},
+    {id: 3, name: 'eggs', quantity: '3 eggs', recipe_id: 2},
+    {id: 4, name: 'cheese', quantity: '1 slice', recipe_id: 3},
+    {id: 5, name: 'bread', quantity: '2 pieces', recipe_id: 3},
+    {id: 6, name: 'butter', quantity: '1 tsb', recipe_id: 3},   
+  });
 };
